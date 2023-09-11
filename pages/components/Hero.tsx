cd192581a00ab0796655f54d0509970b49459a1f
System Administrator <root@Karis-MacBook-Pro.local>
2023-09-11 22:31:34 +0100

    first commit

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -12,29 +12,23 @@ const Hero = () => {
   return (
     <section
     id='home'
-    className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    className='w-full flex justify-center min-h-screen gap-10 max-container text-center  '
   >
-    <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
-      <p className='text-xl font-montserrat text-coral-red'>
+    <div className='relative flex flex-col justify-center w-full  max-xl:padding-x pt-28 text-center'>
+      <p className='text-xl font-montserrat text-coral-red text-center'>
         VOTING APPLICATION
       </p>
 
-      <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
-        <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+      <h1 className='mt-2 font-palanquin text-[130px] max-sm:text-[72px] max-sm:leading-[82px] text-center font-bold'>
+        <span className='xl:whitespace-nowrap relative z-10 pr-10'>
           The New Arrival
         </span>
         <br />
-        <span className='text-coral-red inline-block mt-3'>Vote</span> Wisely 
+        <span className='text-coral-red text-center'>Vote</span> Wisely 
       </h1>
-      <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
-       create a change top the society 
-      </p>
+  
 
-      
-
-      <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
-        Hi
-      </div>
+    
     </div>
 
     
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { NextPage } from "next";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Hero2 from "./components/Hero2";
+import Investment from "./components/Vote";
 
 const Home: NextPage = () => {
   return (
@@ -12,7 +14,13 @@ const Home: NextPage = () => {
     <section className='xl:padding-l wide:padding-r padding-b'>
 <Hero/>
     </section>
-        
+    <section className="padding">
+<Hero2/>
+    </section>
+    {/* <section className="padding">
+<Investment/>
+    </section>
+         */}
        
     
     </main>
